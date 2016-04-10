1a13c3b1d90b20ea5ba656cd7a78fe1650688ad1
alldefector <feng.niu@lattice.io>
2016-04-09 23:54:11 -0700

    Include ddlite as a submodule

--- ddlite/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ddlite import *
-from .ddlite_parser import *
-from .ddlite_matcher import *
-from .ddlite_mindtagger import *
---
+++ include_libs.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(ROOT, 'ddlite'))
