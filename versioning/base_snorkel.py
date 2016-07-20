dfc0ad4e57db101e384052cc119e24dd413a72e3
Jason Alan Fries <jfries@stanford.edu>
2016-07-20 15:31:26 -0700

    added older candidate snapshot version tool

--- versioning/base_snorkel.py
+++ versioning/base_snorkel.py
@@ -1,12 +1,19 @@
+'''
+Snorkel Candidate Version
+
+'''
 import os
-import random
+import sys
+import glob
 import hashlib
+import cPickle
+from ddlite import Relations
 from datetime import datetime
-from ddlite import *
 
 def dict2str(d):
     '''Convert dictionary to tuple pair string'''
     return str(d).encode("utf-8",errors="ignore")
+
 
 def checksum(s):
     '''Create checksum for input object'''
@@ -18,11 +25,12 @@ def checksum(s):
     m.update(s)
     return m.hexdigest()
 
+
 def cands2str(candidates):
     '''Convert DeepDive Relations object to string'''
     convert = lambda x:x.encode("utf-8",errors="ignore")
-    rela_func = lambda x:["{}:{}".format(x.doc_id, x.sent_id)] + map(convert,x.mention1("words")) + map(convert,x.mention2("words"))
-    entity_func = lambda x:["{}:{}".format(x.doc_id, x.sent_id)] + map(convert,x.mention("words"))
+    rela_func = lambda x:["{}:{}".format(x.sentence["doc_id"], x.sentence["sent_id"])] + map(convert,x.mention1("words")) + map(convert,x.mention2("words"))
+    entity_func = lambda x:["{}:{}".format(x.sentence["doc_id"], x.sentence["sent_id"])] + map(convert,x.get_span())
     get_row = rela_func if type(candidates) is Relations else entity_func
     # create string versions of candidates
     s = [":".join(get_row(c)) for c in candidates]
@@ -31,23 +39,50 @@ def cands2str(candidates):
 
 class CandidateVersioner(object):
     '''Create unique version ID for candidate set while saving to disk'''
-    def __init__(self,rootdir,prefix=""):
+    def __init__(self, rootdir, prefix="", dicts={}):
         self.rootdir = rootdir
         self.prefix = prefix
+        self.dicts = dicts
+        self._candidates = {}
         self.filename = None
+        self.checksum = None
+        
+        
+    def snapshot(self, name, candidates):
+        self._candidates[name] = candidates
     
-    def dump_candidates(self, candidates, dicts):
+    
+    def save(self):
         '''Save checksummed version of candidate set. This computes
         checksums based on dictionaries, input documents, and final
         candidate set'''
-        manifest = self._checksums(candidates, dicts)
+        candidates = reduce(lambda x,y:x+y, self._candidates.values())
+        manifest = self._checksums(candidates, self.dicts)
         # dump candidates and log file
-        ctype = "RELATIONS." if type(candidates) is Relations else "ENTITIES."
+        ctype = "RELATIONS." if type(self._candidates) is Relations else "ENTITIES."
         prefix = self.prefix + "." if self.prefix else ""
         self.filename = "{}/{}{}{}".format(self.rootdir,prefix,ctype,manifest["uid"])
-        candidates.dump_candidates("{}.pkl".format(self.filename))
+        
+        cPickle.dump(self._candidates, open("{}.pkl".format(self.filename),"w"))
         self._write_log(self.filename,manifest)
-    
+        self.checksum = manifest["uid"]
+        
+        
+    def load(self,checksum):
+        filelist = glob.glob("{}*{}.pkl".format(self.rootdir,checksum))
+        if len(filelist) > 1:
+            print>>sys.stderr,"Warning: multiple matching checksums"    
+        elif not len(filelist):
+            print>>sys.stderr,"Error: snapshot not found"  
+            return {}  
+        
+        fname = filelist[0]
+        self._candidates = cPickle.load(open(fname,"rb"))
+        self.filename = fname.strip(".pkl")
+        self.checksum = checksum
+        return self._candidates
+
+            
     def _checksums(self, candidates, dicts):
         '''Compute MD5 checksums for all assets used to  
         create this candidate set'''
@@ -56,7 +91,7 @@ class CandidateVersioner(object):
         for name,d in dicts.items():
             manifest["dictionary:{}".format(name)] = checksum(d) 
         # doc and candidate checksum
-        doc_ids = sorted(set([c.doc_id for c in candidates]))
+        doc_ids = sorted(set([c.sentence["doc_id"] for c in candidates]))
         manifest["doc_ids"] = checksum(doc_ids)
         manifest["candidates"] = checksum(cands2str(candidates))
         # some count data about candidates
@@ -68,6 +103,7 @@ class CandidateVersioner(object):
         manifest["uid"] = checksum(reduce(lambda x,y:x+y,values))
         return manifest
     
+    
     def _write_log(self,filename,manifest):
         # write checksums to text file
         ts = datetime.now()
@@ -78,4 +114,5 @@ class CandidateVersioner(object):
                 f.write("{0:<22}{1:^11}{2:<32}\n".format(key,"=",value))
             
         
+
 
